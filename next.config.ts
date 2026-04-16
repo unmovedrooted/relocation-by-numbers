ca31f0aba27f7ba02cf625b97095a69f2f180aea
unmovedrooted <unmovedrooted@gmail.com>
2026-04-16 09:36:45 -0400

    updates

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: "/best-states-for-fire/missouri",
         permanent: true,
       },
+      {
+        source: "/cost-of-living-in/:cityId",
+        destination: "/cost-of-living/:cityId",
+        permanent: true,
+      },
     ];
   },
 };
